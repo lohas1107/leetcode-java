21c52be1c86b7b40196827237983d05f836a8728
Yarr <lohas1107@gmail.com>
2019-06-30 16:48:48 +0800

    fix unit test

--- src/test/java/dev/hello/leetcode/jewels_and_stones/SolutionTest.java
+++ src/test/java/dev/hello/leetcode/jewels_and_stones/SolutionTest.java
@@ -9,9 +9,6 @@ class SolutionTest {
     @Test
     void numJewelsInStones() {
         Solution solution = new Solution();
-        assertEquals(0, solution.numJewelsInStones("aA", null));
-        assertEquals(0, solution.numJewelsInStones(null, "aAAbbbb"));
-        assertEquals(0, solution.numJewelsInStones(null, null));
         assertEquals(3, solution.numJewelsInStones("aA", "aAAbbbb"));
         assertEquals(0, solution.numJewelsInStones("z", "ZZ"));
     }
