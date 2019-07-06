60f5fcfebd68698760148000676380a4d411f73d
Yarr <lohas1107@gmail.com>
2019-07-06 16:53:55 +0800

    more elegant solution

--- src/main/java/dev/hello/leetcode/binary_search_tree_to_greater_sum_tree/Solution.java
+++ src/main/java/dev/hello/leetcode/binary_search_tree_to_greater_sum_tree/Solution.java
@@ -2,28 +2,14 @@ package dev.hello.leetcode.binary_search_tree_to_greater_sum_tree;
 
 import dev.hello.structures.TreeNode;
 
-import java.util.Stack;
-
 class Solution {
+
+    private int sum = 0;
+
     public TreeNode bstToGst(TreeNode root) {
-
-        Stack<TreeNode> treeNodeStack = new Stack<>();
-        TreeNode node = root;
-        int sum = 0;
-
-        while (node != null || !treeNodeStack.isEmpty()) {
-            while (node != null) {
-                treeNodeStack.push(node);
-                node = node.right;
-            }
-
-            node = treeNodeStack.pop();
-            sum += node.val;
-            node.val = sum;
-
-            node = node.left;
-        }
-
+        if (root.right != null) bstToGst(root.right);
+        sum = root.val = sum + root.val;
+        if (root.left != null) bstToGst(root.left);
         return root;
     }
 }
