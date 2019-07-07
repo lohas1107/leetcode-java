381984f216ff2977b255ae99f69cf17c18c11b68
Yarr <lohas1107@gmail.com>
2019-07-07 20:23:33 +0800

    improve time performance

--- src/main/java/dev/hello/leetcode/range_sum_of_bst/Solution.java
+++ src/main/java/dev/hello/leetcode/range_sum_of_bst/Solution.java
@@ -4,12 +4,10 @@ import dev.hello.structures.TreeNode;
 
 class Solution {
 
-    private int rangeSum = 0;
-
     public int rangeSumBST(TreeNode root, int L, int R) {
-        if (root.val >= L && root.val <= R) rangeSum += root.val;
-        if (root.left != null ) rangeSumBST(root.left, L, R);
-        if (root.right != null ) rangeSumBST(root.right, L, R);
-        return rangeSum;
+        if (root == null) return 0;
+        if (root.val < L) return rangeSumBST(root.right, L , R);
+        if (root.val > R) return rangeSumBST(root.left, L, R);
+        return  root.val + rangeSumBST(root.right, L , R) + rangeSumBST(root.left, L, R);
     }
 }
