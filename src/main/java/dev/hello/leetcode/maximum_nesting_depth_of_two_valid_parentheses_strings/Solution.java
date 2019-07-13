411662020300d9ce6da016afe5b7054ff19c38c0
Yarr <lohas1107@gmail.com>
2019-07-13 14:35:22 +0800

    super elegant solution

--- src/main/java/dev/hello/leetcode/maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.java
+++ src/main/java/dev/hello/leetcode/maximum_nesting_depth_of_two_valid_parentheses_strings/Solution.java
@@ -3,16 +3,9 @@ package dev.hello.leetcode.maximum_nesting_depth_of_two_valid_parentheses_string
 class Solution {
     public int[] maxDepthAfterSplit(String seq) {
         int[] depth = new int[seq.length()];
-        int group = -1;
 
         for (int i = 0; i < seq.length(); i++) {
-            if (seq.charAt(i) == '(') {
-                group++;
-                depth[i] = group % 2;
-            } else {
-                depth[i] = group % 2;
-                group--;
-            }
+            depth[i] = (seq.charAt(i) == '(') ? i & 1 : (1 - i & 1);
         }
 
         return depth;
